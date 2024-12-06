e53c8a18d30996d7d7f23586bab440c299822858
YMolodyko <VTB4081847@region.vtb.ru>
2024-12-06 14:32:06 +0300

    fix front

--- front/app.js
+++ front/app.js
@@ -1,60 +1,62 @@
-let pizzaUrl = 'http://localhost:8089/pizza';
-let orderUrl = 'http://localhost:8089/order';
+let pizzaUrl = "http://localhost:8089/pizza";
+let orderUrl = "http://localhost:8089/order";
 
-let pizzaMenu = document.getElementById('pizza-menu');
-pizzaMenu.innerHTML = null;
-
-let pizzas2 = fetch(pizzaUrl).then(e => e.json())
-    .then(e => addPizzas(e))
-    .then( () => addEventListenerToPizza());
+let pizzas2 = fetch(pizzaUrl)
+  .then((response) => response.json())
+  .then((body) => addPizzas(body))
+  .then(() => addEventListenerToPizza());
 
 function addPizzas(pizzas) {
-    pizzaMenu.innerHTML = pizzas.map(pizza => createPizzaCard(pizza)).join("");
+  let pizzaMenu = document.getElementById("pizza-menu");
+  pizzaMenu.innerHTML = pizzas.map((pizza) => createPizzaCard(pizza)).join("");
 }
 
-let pizzaBtns = document.getElementsByClassName('pizza-btn');
+let pizzaBtns = document.getElementsByClassName("pizza-btn");
 console.log(pizzaBtns);
-let resultBucketHtml = document.getElementById('bucket');
+let resultBucketHtml = document.getElementById("bucket");
 let bucket = {};
-let makeOrderBtn = document.getElementById('make-order');
-
+let makeOrderBtn = document.getElementById("make-order");
 
 function addEventListenerToPizza() {
-    Array.prototype.slice.call(pizzaBtns).forEach(element => { element.addEventListener('click', function() {
-            bucket[element.name] = (bucket[element.name] ?? 0) + 1;
-            const bucketList = Object.entries(bucket)
-                .map(([k, v]) => `<li class="list-group-item">${k} - ${v} шт.</li>`)
-                .join("");
-            resultBucketHtml.innerHTML = bucketList;
-            makeOrderBtn.style.display = 'block';
-        });
+  Array.prototype.slice.call(pizzaBtns).forEach((element) => {
+    element.addEventListener("click", function () {
+      bucket[element.name] = (bucket[element.name] ?? 0) + 1;
+      const bucketList = Object.entries(bucket)
+        .map(([k, v]) => `<li class="list-group-item">${k} - ${v} шт.</li>`)
+        .join("");
+      resultBucketHtml.innerHTML = bucketList;
+      makeOrderBtn.style.display = "block";
     });
+  });
 }
 
-makeOrderBtn.addEventListener('click', async function() {
-    let response = await fetch(orderUrl, {
-        method: 'POST',
-        body: JSON.stringify(bucket)
-    })    
+makeOrderBtn.addEventListener("click", async function () {
+  let response = await fetch(orderUrl, {
+    method: "POST",
+    body: JSON.stringify(bucket),
+  })
     .then((number) => successOrder(number))
-    .catch(() => showElementOnTime('order-fail', 3000));
+    .catch(() => showElementOnTime("order-fail", 3000));
 });
 
-function successOrder(number) {
-    showElementOnTime("order-success", 3000, number);
-    resultBucketHtml.innerHTML = 'Ничего не выбрано';
-    let orderEl = document.getElementById(elementId);
-    orderEl.innerHTML = `Заказ успешно принят. Номер ${number}`;
+async function successOrder(response) {
+  let number = await response.text();
+  let orderEl = document.getElementById("order-success");
+  orderEl.innerHTML = `Заказ успешно принят. Номер ${number}`;
+  showElementOnTime("order-success", 3000);
+  resultBucketHtml.innerHTML = "Ничего не выбрано";
 }
 
-function showElementOnTime (elementId, time) {
-    let classList = document.getElementById(elementId).classList;
-    classList.toggle('d-none');    
-    setTimeout(() => {classList.toggle('d-none')}, time);
+function showElementOnTime(elementId, time) {
+  let classList = document.getElementById(elementId).classList;
+  classList.toggle("d-none");
+  setTimeout(() => {
+    classList.toggle("d-none");
+  }, time);
 }
 
 function createPizzaCard(pizza) {
-    return `
+  return `
   <div class="card me-3" style="width: 18rem">
           <img
             src="${pizza.img_url}"
@@ -77,5 +79,5 @@ function createPizzaCard(pizza) {
             <span class="ms-2">${pizza.price} рублей</span>
           </div>
         </div>
-`
+`;
 }
